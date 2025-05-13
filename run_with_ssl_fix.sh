9676f25ca0538b3a2420c9946abbead6a8cbad5c
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-05-13 13:01:03 +0200

    Remove the `run_with_ssl_fix.sh` script as it is no longer needed for running the ZenML pipeline with SSL certificate fix. This cleanup helps streamline the project by eliminating obsolete files.

--- run_with_ssl_fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run ZenML pipeline with SSL certificate fix
-
-# Set SSL certificate paths
-export SSL_CERT_FILE=$(python -c "import certifi; print(certifi.where())")
-export REQUESTS_CA_BUNDLE=$(python -c "import certifi; print(certifi.where())")
-
-# Run the pipeline
-python gpu_pipeline.py

ef8598c46d067dd3510e7b868efcf48dd33a8be0
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2025-05-15 09:38:13 +0200

    Refactor format.sh script to improve argument handling and processing
    - Changed the way command-line arguments are handled by using an array to store source paths, enhancing flexibility for users.
    - Updated the script to consistently use the new array for both the 'ruff check' and 'ruff format' commands, ensuring that all specified source paths are processed correctly.
    
    This update aims to enhance the usability and maintainability of the formatting script.

--- scripts/format.sh
+++ scripts/format.sh
@@ -12,18 +12,18 @@ default_src="."
 # Check if command-line arguments are provided
 if [ $# -gt 0 ]; then
     # Use all command-line arguments as source paths
-    SRC="$@"
+    SRC_PATHS=("$@")
 else
     # Use default source directory if no arguments provided
-    SRC="$default_src"
+    SRC_PATHS=("$default_src")
 fi
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
 
 # autoflake replacement: removes unused imports and variables
-ruff check $SRC --select F401,F841 --fix --exclude "__init__.py" --isolated
+ruff check "${SRC_PATHS[@]}" --select F401,F841 --fix --exclude "__init__.py" --isolated
 
 # sorts imports
-ruff check $SRC --select I --fix --ignore D
-ruff format $SRC
+ruff check "${SRC_PATHS[@]}" --select I --fix --ignore D
+ruff format "${SRC_PATHS[@]}"
